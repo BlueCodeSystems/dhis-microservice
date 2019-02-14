5000fe4e2945eaa232bb8285abeac141461e70ca
Zita <atembo@bluecodeltd.com>
2019-02-14 12:44:36 +0200

    Query HIA reports

--- script.py
+++ script.py
@@ -3,3 +3,9 @@ import psycopg2
 
 #db connection
 conn = psycopg2.connect(host=config.POSTGRES_HOST,database=config.POSTGRES_DB, user=config.POSTGRES_USER, password=config.POSTGRES_PASS)
+cur = conn.cursor()
+sql="SELECT * FROM public.couchdb where doc @> '{\"type\":\"Report\"}';"
+
+cur.execute(sql)
+
+print(cur.fetchall())
