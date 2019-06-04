fcbc482fd244f9c8a29cffe8986a5b18c9c45f82
Ubuntu <atembo@bluecodeltd.com>
2019-06-04 10:45:21 +0000

    Add report period parameter

--- script/script.py
+++ script/script.py
@@ -3,13 +3,18 @@ import psycopg2
 import sys
 import json
 import requests
+import datetime
+
+#log time 
+print('\n\n','['+datetime.datetime.now().strftime('%c')+']')
 
 try:
     report_month = sys.argv[1]
 except:
     report_month = None
 
-rest_api_endpoint = '/api/26/dataValueSets'
+dhis_credentials = (config.DHIS_USER,config.DHIS_PASS)
+data_elements_id = []
 
 #db connection
 conn = psycopg2.connect(host=config.POSTGRES_HOST,database=config.POSTGRES_DB, user=config.POSTGRES_USER, password=config.POSTGRES_PASS)
@@ -19,6 +24,13 @@ cur = conn.cursor()
 sql="SELECT * FROM public.couchdb where doc @> '{\"type\":\"Report\"}';"
 cur.execute(sql)
 result = cur.fetchall()
+
+#Retrieve data element ids
+url = config.DHIS_HOST+config.DHIS_DATASET_REST_API_ENDPOINT+'/'+config.DHIS_DATASET
+r = requests.get(url, auth=dhis_credentials)
+response = r.json()
+
+data_element_ids = [d['dataElement']['id'] for d in response['dataSetElements']]
 
 def clean_indicator(indicator):
     if(indicator["dhisId"] !=''):
@@ -41,16 +53,27 @@ def filter_report_by_month(month):
     return f 
     
 
-def parse_to_dhis_dataelement_json_payload(reports):
-    dataelements = []
-    for report in reports:
-        if(report != None):
-            org_unit_id = report["orgUnitId"]
-            period = report["reportDate"][:7].replace("-","")
-            for indicator in report["hia2Indicators"]:
-               if(indicator != None):
-                   dataelements.append({"dataElement":indicator["dhisId"],"period":period,"orgUnit":org_unit_id,"value":indicator["value"]})
-    return json.dumps({"dataValues":dataelements})
+def parse_to_dhis_dataelement_json_payload(report,dataelement_set):
+    if(report != None):
+        report_date = report['reportDate'][:10]
+        data_elements = []
+        
+        #Populate required data elements with 0 as defualt value
+        for dataelement_id in dataelement_set:
+            data_elements.append({"dataElement":dataelement_id,"value":"0"})
+
+        #Update data element value from indicator values
+        for indicator in report['hia2Indicators']:
+                if(indicator != None):
+                     for data_element in data_elements:
+                         if(indicator['dhisId'] == data_element['dataElement']):
+                             data_element['value'] = indicator['value']
+
+        return {"dataSet":config.DHIS_DATASET,
+                "completeDate":report_date,
+                "period":report_date[:7].replace("-",""),
+                "orgUnit":report['orgUnitId'],
+                "dataValues":data_elements}
 
 cleaned_reports = map(clean_report,[row[0] for row in result])
 
@@ -59,9 +82,10 @@ if(report_month !=  None):
 else:
     final_reports = cleaned_reports
 
-json_payload = parse_to_dhis_dataelement_json_payload(final_reports)
-
 #post reports to DHIS
-url = config.DHIS_HOST+rest_api_endpoint
-
-r = requests.post(url, auth=(config.DHIS_USER,config.DHIS_PASS), json=json_payload)
+url = config.DHIS_HOST+config.DHIS_DATA_VALUE_SET_REST_API_ENDPOINT
+for report in final_reports:
+   if(report != None):
+       json_payload = parse_to_dhis_dataelement_json_payload(report,data_element_ids)
+       r = requests.post(url, auth=dhis_credentials, json=json_payload, headers={"Content-Type":"application/json"})
+       print(r.json())
--- start.sh
+++ start.sh
@@ -1,18 +1,29 @@
-#!/bash/sh
+#!/bin/bash
 
 #set script to execute
 export SCRIPT=script.py
 
 #set enviromental variables
 export POSTGRES_USER=postgres
-export POSTGRES_PASS=password
-export POSTGRES_DB=opensrp
+export POSTGRES_PASS=XXXXXXXXXXXXXXXXXXX
+export POSTGRES_DB=db_name
 export POSTGRES_HOST=127.0.0.1
-export DHIS_USER=admin
-export DHIS_PASS=pass
-export DHIS_HOST=https://dhis.zeir-stage.smartregister.org
+export DHIS_USER=dhis_username
+export DHIS_PASS=XXXXXXXXXXXXXX
+export DHIS_HOST=https://dhis.example.org
+export DHIS_DATASET=KD5kmrkn5
+export DHIS_DATASET_REST_API_ENDPOINT=/api/26/dataSets
+export DHIS_DATA_VALUE_SET_REST_API_ENDPOINT=/api/26/dataValueSets
+export REPORT_MONTH=`date +%Y-%m`
+export LOG_FILE=./logs
 
-python script/$SCRIPT `date +%Y-%m`
+if [ ! -e $LOG_FILE ]; then
+    touch $LOG_FILE
+fi
+
+exec 3>&1 1>>${LOG_FILE} 2>&1
+
+env/bin/python3.7 script/$SCRIPT $REPORT_MONTH
 
 unset POSTGRES_USER
 unset POSTGRES_PASS
