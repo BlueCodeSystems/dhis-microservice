044f5c28e39ba95b73304492a80b551f9dba754f
makili <makilimusepa@gmail.com>
2019-08-12 14:21:19 +0200

    update aggregated function call

--- script/etlscript.py
+++ script/etlscript.py
@@ -8,6 +8,7 @@ try:
         print("Connected to database ", db_Info)
         cursor = connection.cursor(dictionary=True)
 
+    # Function definitions
     #Get visit ids that satisfy a given indicator
     def visitList(indicator, visitTypeId, locationId, visitMonth):
         query = "SELECT visit_id FROM visit_data_matvw WHERE obs_concept_id = {} AND obs_value_concept_id = {} AND visit_type_id = {} AND visit_location_id = {} AND DATE_FORMAT(date_started, '%m-%Y') = '{}' AND visit_location_retired = 0".format(indicator["question"], indicator["answer"], visitTypeId, locationId, visitMonth)
@@ -56,34 +57,66 @@ try:
 
 
 
-    visits1 = visitList({"question":165182, "answer":165183}, 5, 5314, '08-2019')
-#    print(visits1)
-#    print("-------------------------------------------------------------------")
+    # Function calls
 
-    visits2 = visitList({"question":165203, "answer":165125}, 5, 5314, '08-2019')
-#    print(visits2)
-#    print("-------------------------------------------------------------------")
+    #No. of patients referred for suspect cancer on one year follow-up visit with Unknown HIV status
+    print("No. of patients referred for suspect cancer on one year follow-up visit with Unknown HIV status")
+    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165132}]
+    print(patientCount(concepts, 5, 5314, '08-2019'))
 
-    visits3 = visitList({"question":165223, "answer":165126}, 5, 5314, '08-2019')
-#    print(visits3)
-    print("---------------------Indicators---------------------------")
+    #No. of patients referred for suspect cancer on one year follow-up visit and are HIV negative
+    print("No. of patients referred for suspect cancer on one year follow-up visit and are HIV negative")
+    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165131}]
+    print(patientCount(concepts, 5, 5314, '08-2019'))    
 
-    indicators = listIntersection([visits1, visits2, visits3])
-#    print(indicators)
+    #No. of patients referred for suspect cancer on one year follow-up visit and are HIV positive
+    print("No. of patients referred for suspect cancer on one year follow-up visit and are HIV positive")
+    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165125}]
+    print(patientCount(concepts, 5, 5314, '08-2019'))
 
-    print("------------------------Total patients on ART----------------------------")
-    patients = patientList(indicators)
-    print(patients)
+    #No. of patients referred for suspect cancer on one year follow-up visit and are not on ART
+    print("No. of patients referred for suspect cancer on one year follow-up visit and are not on ART")
+    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165125}, {"question":165223, "answer":165127}]
+    print(patientCount(concepts, 5, 5314, '08-2019'))
 
-    print("######################################################################")
-
-    if isinstance(patients, list):
-        print("----------------HIV +ve clients on art in age range --------------")
-        ages = patientAges(patientList(indicators), 0, 24)
-        print(ages)
-    
-    print("Patients referred for suspcect cancer on initial visit and HIV unknown")
+    #No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age <= 24
+    print("No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age <= 24")
     concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165125}, {"question":165223, "answer":165126}]
+    print(patientCount(concepts, 5, 5314, '08-2019', 0, 24))
+
+    #No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age between 25-29
+    print("No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age between 25-29")
+    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165125}, {"question":165223, "answer":165126}]
+    print(patientCount(concepts, 5, 5314, '08-2019', 25, 29))
+
+    #No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age between 30-34
+    print("No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age between 30-34")
+    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165125}, {"question":165223, "answer":165126}]
+    print(patientCount(concepts, 5, 5314, '08-2019', 30, 34))
+
+    #No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age between 40-49
+    print("No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age between 40-49")
+    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165125}, {"question":165223, "answer":165126}]
+    print(patientCount(concepts, 5, 5314, '08-2019', 40, 49))
+
+    #No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age between 50-59
+    print("No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age between 50-59")
+    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165125}, {"question":165223, "answer":165126}]
+    print(patientCount(concepts, 5, 5314, '08-2019', 50, 59))
+
+    #No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age >= 60
+    print("No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age >= 60")
+    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165125}, {"question":165223, "answer":165126}]
+    print(patientCount(concepts, 5, 5314, '08-2019', 60, 110))
+
+    #Total No. of patients referred for suspect cancer on one year follow-up visit who are on ART
+    print("Total No. of patients referred for suspect cancer on one year follow-up visit who are on ART")
+    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165125}, {"question":165223, "answer":165126}]
+    print(patientCount(concepts, 5, 5314, '08-2019'))
+
+    #Total No. of patients referred for suspect cancer on one year follow-up visit
+    print("Total No. of patients referred for suspect cancer on one year follow-up visit")
+    concepts = [{"question":165182, "answer":165183}]
     print(patientCount(concepts, 5, 5314, '08-2019'))
 
 
