a909ed25bb9434b21859cdf72bf3419048741e1b
makili <makilimusepa@gmail.com>
2019-08-13 11:57:36 +0200

    create single function for each indicator row

--- script/etlscript.py
+++ script/etlscript.py
@@ -5,13 +5,14 @@ try:
     connection = mysql.connector.connect(host = '34.240.241.171', database = 'openmrs', user = 'smartcerv', password = 'smartcerv')
     if connection.is_connected():
         db_Info = connection.get_server_info()
-        print("Connected to database ", db_Info)
+        print('Connected to database ', db_Info)
         cursor = connection.cursor(dictionary=True)
+        cursor.execute('CALL RefreshMaterializedViews()')
 
     # Function definitions
     #Get visit ids that satisfy a given indicator
     def visitList(indicator, visitTypeId, locationId, visitMonth):
-        query = "SELECT visit_id FROM visit_data_matvw WHERE obs_concept_id = {} AND obs_value_concept_id = {} AND visit_type_id = {} AND visit_location_id = {} AND DATE_FORMAT(date_started, '%m-%Y') = '{}' AND visit_location_retired = 0".format(indicator["question"], indicator["answer"], visitTypeId, locationId, visitMonth)
+        query = "SELECT visit_id FROM visit_data_matvw WHERE obs_concept_id = {} AND obs_value_concept_id = {} AND visit_type_id = {} AND visit_location_id = {} AND DATE_FORMAT(date_started, '%m-%Y') = '{}' AND visit_location_retired = 0".format(indicator['question'], indicator['answer'], visitTypeId, locationId, visitMonth)
         cursor.execute(query)
         visitIdsList = cursor.fetchall()
         result = []
@@ -25,7 +26,10 @@ try:
 
     #Get patient ids from list of visit ids
     def patientList(visitIds):
-        query = 'SELECT DISTINCT patient_id FROM visit_data_matvw WHERE visit_id IN {}'.format(tuple(visitIds))
+        if (len(visitIds) > 1):
+            query = 'SELECT DISTINCT patient_id FROM visit_data_matvw WHERE visit_id IN {}'.format(tuple(visitIds))
+        else:
+            query = 'SELECT DISTINCT patient_id FROM visit_data_matvw WHERE visit_id = {}'.format(visitIds[0])
         cursor.execute(query)
         patientIds = cursor.fetchall()
         result = []
@@ -35,93 +39,95 @@ try:
 
     # Filter patients by age
     def patientAges(patientIds, lowerLimit, upperLimit):
-        query = 'SELECT COUNT(DISTINCT patient_id) AS patient_id FROM patient_data_matvw WHERE patient_id IN {} AND TIMESTAMPDIFF(YEAR, birthdate, CURDATE()) BETWEEN {} AND {}'.format(tuple(patientIds), lowerLimit, upperLimit)
+        if (len(patientIds) > 1):
+            query = 'SELECT COUNT(DISTINCT patient_id) AS patient_id FROM patient_data_matvw WHERE patient_id IN {} AND TIMESTAMPDIFF(YEAR, birthdate, CURDATE()) BETWEEN {} AND {}'.format(tuple(patientIds), lowerLimit, upperLimit)
+        else:
+            query = 'SELECT COUNT(DISTINCT patient_id) AS patient_id FROM patient_data_matvw WHERE patient_id = {} AND TIMESTAMPDIFF(YEAR, birthdate, CURDATE()) BETWEEN {} AND {}'.format(patientIds[0], lowerLimit, upperLimit)
         cursor.execute(query)
         patientsInRange = cursor.fetchall()
         result = []
         for visit in patientsInRange:
             result.append(visit['patient_id'])
-        return sorted(result)
+        return result[0]
 
     # Aggregated function call
-    def patientCount(concepts, visitTypeId, locationId, visitMonth, lowerAgeLimit = -1, upperAgeLimit = -1):
+    def patientCount(questionAnswer, visitTypeId, locationId, visitMonth, lowerAgeLimit = -1, upperAgeLimit = -1):
         listOfVisits = []
-        for questAns in concepts:
+        for questAns in questionAnswer:
             listOfVisits.append(visitList(questAns, visitTypeId, locationId, visitMonth))
         indicators = listIntersection(listOfVisits)
         patients = patientList(indicators)
         if (lowerAgeLimit == -1 or upperAgeLimit == -1):
             return len(patients)
         else: 
-            return patientAges(patientList(indicators), lowerAgeLimit, upperAgeLimit)
+            return patientAges(patients, lowerAgeLimit, upperAgeLimit)
 
 
-    # Function calls
 
-    #No. of patients referred for suspect cancer on one year follow-up visit with Unknown HIV status
-    print("No. of patients referred for suspect cancer on one year follow-up visit with Unknown HIV status")
-    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165132}]
-    print(patientCount(concepts, 5, 5314, '08-2019'))
 
-    #No. of patients referred for suspect cancer on one year follow-up visit and are HIV negative
-    print("No. of patients referred for suspect cancer on one year follow-up visit and are HIV negative")
-    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165131}]
-    print(patientCount(concepts, 5, 5314, '08-2019'))    
 
-    #No. of patients referred for suspect cancer on one year follow-up visit and are HIV positive
-    print("No. of patients referred for suspect cancer on one year follow-up visit and are HIV positive")
-    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165125}]
-    print(patientCount(concepts, 5, 5314, '08-2019'))
+    #Suspect cancer - initial visit
+    
+    def indicatorRow(listOfIndicators, visitTypeId, visitLocationId, visitMonth):
+        concepts1 = listOfIndicators.copy()
+        concepts1.extend([{'question':165203, 'answer':165132}])
+        print('Hiv unknown - {}'.format(patientCount(concepts1, visitTypeId, visitLocationId, visitMonth)))
 
-    #No. of patients referred for suspect cancer on one year follow-up visit and are not on ART
-    print("No. of patients referred for suspect cancer on one year follow-up visit and are not on ART")
-    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165125}, {"question":165223, "answer":165127}]
-    print(patientCount(concepts, 5, 5314, '08-2019'))
+        concepts1 = listOfIndicators.copy()
+        concepts1.extend([{'question':165203, 'answer':165131}])
+        print('Hiv negative - {}'.format(patientCount(concepts1, visitTypeId, visitLocationId, visitMonth)))
 
-    #No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age <= 24
-    print("No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age <= 24")
-    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165125}, {"question":165223, "answer":165126}]
-    print(patientCount(concepts, 5, 5314, '08-2019', 0, 24))
+        concepts1 = listOfIndicators.copy()
+        concepts1.extend([{'question':165203, 'answer':165125}])
+        print('Hiv positive - {}'.format(patientCount(concepts1, visitTypeId, visitLocationId, visitMonth)))
 
-    #No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age between 25-29
-    print("No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age between 25-29")
-    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165125}, {"question":165223, "answer":165126}]
-    print(patientCount(concepts, 5, 5314, '08-2019', 25, 29))
+        concepts1 = listOfIndicators.copy()
+        concepts1.extend([{'question':165203, 'answer':165125}, {'question':165223, 'answer':165127}])
+        print('Not on ART - {}'.format(patientCount(concepts1, visitTypeId, visitLocationId, visitMonth)))
 
-    #No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age between 30-34
-    print("No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age between 30-34")
-    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165125}, {"question":165223, "answer":165126}]
-    print(patientCount(concepts, 5, 5314, '08-2019', 30, 34))
+        concepts1 = listOfIndicators.copy()
+        concepts1.extend([{'question':165203, 'answer':165125}, {'question':165223, 'answer':165126}])
+        print('Positive, on ART - <= 24 - {}'.format(patientCount(concepts1, visitTypeId, visitLocationId, visitMonth, 0, 24)))
 
-    #No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age between 40-49
-    print("No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age between 40-49")
-    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165125}, {"question":165223, "answer":165126}]
-    print(patientCount(concepts, 5, 5314, '08-2019', 40, 49))
+        concepts1 = listOfIndicators.copy()
+        concepts1.extend([{'question':165203, 'answer':165125}, {'question':165223, 'answer':165126}])
+        print('Positive, on ART - 25-29 - {}'.format(patientCount(concepts1, visitTypeId, visitLocationId, visitMonth, 25, 29)))
 
-    #No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age between 50-59
-    print("No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age between 50-59")
-    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165125}, {"question":165223, "answer":165126}]
-    print(patientCount(concepts, 5, 5314, '08-2019', 50, 59))
+        concepts1 = listOfIndicators.copy()
+        concepts1.extend([{'question':165203, 'answer':165125}, {'question':165223, 'answer':165126}])
+        print('Positive, on ART - 30-34 - {}'.format(patientCount(concepts1, visitTypeId, visitLocationId, visitMonth, 30, 34)))
 
-    #No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age >= 60
-    print("No. of patients referred for suspect cancer on one year follow-up visit who are HIV +ve, on ART and age >= 60")
-    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165125}, {"question":165223, "answer":165126}]
-    print(patientCount(concepts, 5, 5314, '08-2019', 60, 110))
+        concepts1 = listOfIndicators.copy()
+        concepts1.extend([{'question':165203, 'answer':165125}, {'question':165223, 'answer':165126}])
+        print('Positive, on ART - 35-39 - {}'.format(patientCount(concepts1, visitTypeId, visitLocationId, visitMonth, 35, 39)))
 
-    #Total No. of patients referred for suspect cancer on one year follow-up visit who are on ART
-    print("Total No. of patients referred for suspect cancer on one year follow-up visit who are on ART")
-    concepts = [{"question":165182, "answer":165183}, {"question":165203, "answer":165125}, {"question":165223, "answer":165126}]
-    print(patientCount(concepts, 5, 5314, '08-2019'))
+        concepts1 = listOfIndicators.copy()
+        concepts1.extend([{'question':165203, 'answer':165125}, {'question':165223, 'answer':165126}])
+        print('Positive, on ART - 40-49 - {}'.format(patientCount(concepts1, visitTypeId, visitLocationId, visitMonth, 40, 49)))
 
-    #Total No. of patients referred for suspect cancer on one year follow-up visit
-    print("Total No. of patients referred for suspect cancer on one year follow-up visit")
-    concepts = [{"question":165182, "answer":165183}]
-    print(patientCount(concepts, 5, 5314, '08-2019'))
+        concepts1 = listOfIndicators.copy()
+        concepts1.extend([{'question':165203, 'answer':165125}, {'question':165223, 'answer':165126}])
+        print('Positive, on ART - 50-59 - {}'.format(patientCount(concepts1, visitTypeId, visitLocationId, visitMonth, 50, 59)))
+
+        concepts1 = listOfIndicators.copy()
+        concepts1.extend([{'question':165203, 'answer':165125}, {'question':165223, 'answer':165126}])
+        print('Positive, on ART - >= 60 - {}'.format(patientCount(concepts1, visitTypeId, visitLocationId, visitMonth, 60, 110)))
+
+        concepts1 = listOfIndicators.copy()
+        concepts1.extend([{'question':165203, 'answer':165125}, {'question':165223, 'answer':165126}])
+        print('Total on ART - {}'.format(patientCount(concepts1, visitTypeId, visitLocationId, visitMonth)))
+
+        concepts1 = listOfIndicators.copy()
+        print('Total suspect cancer - {}'.format(patientCount(concepts1, visitTypeId, visitLocationId, visitMonth)))
+
+    print('###############################')
+
+    indicatorRow([{'question':165182, 'answer':165183}], 5, 5314, '08-2019')
 
 
 except Error as e:
-    print("Error while connecting to database: ", e)
+    print('Error while connecting to database: ', e)
 finally:
     if connection.is_connected():
         connection.close()
-        print("Database connection closed.")
+        print('Database connection closed.')
